48cc47e59412b0c53ceb11a84c53c7fa40fb89b7
Martin Alcock <martin.alcock@forgerock.com>
2023-01-13 15:37:43 +0000

    add argo picker

--- lua/telescope/_extensions/fraas.lua
+++ lua/telescope/_extensions/fraas.lua
@@ -18,11 +18,14 @@ local M = {
     io_account = nil,
     sa_account = nil,
     terminal_cmd = nil,
-    staging_context = nil,
+    forge_shell_cmd = nil,
+    argo_watch_cmd = nil,
+    staging_conext = nil,
+    argo_context = nil,
   }
 }
 
-local test_state = {
+local states = {
   Running = "🚀",
   Finished = "🏁",
   Failed = "❌",
@@ -87,6 +90,22 @@ local get_fraas_tests = function()
   return entries
 end
 
+local get_orch_runs = function()
+  local cwd = vim.fn.getcwd()
+  local results, _, stderr = get_os_command_output({ "argo", "list", "--context", M.opts.argo_context, "--namespace",
+    "argo" }, cwd)
+  local entries = {}
+  for _, workflow in ipairs(results) do
+    local name, state, age, duration, pri, message = string.match(workflow,
+      "([%w%p]+)%s+(%w+)%s+(%w+)%s+(%w+)%s+(%d+)%s+(.*)")
+    if name ~= "NAME" then
+      table.insert(entries, { name, state, age, duration, pri, message })
+    end
+  end
+  print(vim.inspect(results))
+  return entries
+end
+
 local open_gcp_console = function(account, id)
   vim.api.nvim_command(string.format("OpenBrowser https://console.cloud.google.com/getting-started?authuser=%s&project=%s"
     , account, id))
@@ -113,7 +132,7 @@ M.fraas_projects = function(opts)
       actions.select_default:replace(function()
         -- actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        vim.fn.system(string.format(M.opts.terminal_cmd, selection.name, selection.name))
+        vim.fn.system(string.format(M.opts.terminal_cmd .. M.opts.forge_shell_cmd, selection.name, selection.name))
       end)
       actions.open_io_gcp_console = function()
         -- actions.close(prompt_bufnr)
@@ -149,7 +168,7 @@ M.fraas_tests = function(opts)
       entry_maker = function(entry)
         return {
           value = entry[1],
-          display = string.format("%s\t-\t%s\t-\t%s", test_state[entry[2]], entry[1], entry[4]),
+          display = string.format("%s\t-\t%s\t-\t%s", states[entry[2]], entry[1], entry[4]),
           ordinal = entry[4],
           slackThread = entry[5],
           id = entry[1],
@@ -185,27 +204,29 @@ M.fraas_tests = function(opts)
         local selection = action_state.get_selected_entry()
         vim.fn.system(string.format("kubectl --context %s delete testrun %s", M.staging_context, selection.name))
       end
+
+      map('n', 'x', actions.terminate_testrun)
+
       return true
     end,
   }):find()
 end
 
-M.fraas_orch = function(opts)
+M.fraas_argo = function(opts)
   pickers.new(opts, {
     prompt_title = string.format("FRaaS Argo Workflow runs"),
     finder = finders.new_table {
-      results = get_fraas_tests(),
+      results = get_orch_runs(),
       entry_maker = function(entry)
         return {
           value = entry[1],
-          display = string.format("%s\t-\t%s\t-\t%s", test_state[entry[2]], entry[1], entry[4]),
-          ordinal = entry[4],
-          slackThread = entry[5],
-          id = entry[1],
-          state = entry[2],
-          branch = entry[3],
-          name = entry[6],
-          created = entry[7],
+          display = string.format("%s\t%s\t%s\t%s\t%s", entry[1], states[entry[2]], entry[3], entry[4], entry[6]),
+          ordinal = entry[1],
+          name = entry[1],
+          age = entry[3],
+          duration = entry[4],
+          pri = entry[5],
+          message = entry[6],
         }
       end
     },
@@ -214,11 +235,13 @@ M.fraas_orch = function(opts)
       title = "Test run details",
       define_preview = function(self, entry, status)
         vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, {
-          string.format("Name:\t\t\t%s", entry.name),
-          string.format("ID:\t\t\t\t%s", entry.id),
-          string.format("Branch:\t\t%s", entry.branch),
-          string.format("State:\t\t%s", entry.state),
-          string.format("Created:\t\t%s", entry.created),
+          string.format("Name:\t\t\t\t%s", entry.name),
+          string.format("Age:\t\t\t\t%s", entry.age),
+          string.format("Duration:\t\t%s", entry.duration),
+          string.format("Priority:\t\t%s", entry.pri),
+          string.format("Message:"),
+          "------------------------------",
+          entry.message,
         })
       end
     },
@@ -226,31 +249,40 @@ M.fraas_orch = function(opts)
       actions.select_default:replace(function()
         -- actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        vim.api.nvim_command(string.format("OpenBrowser https://forgerock.slack.com/archives/C010WEFEMRV/p%s"
-          , selection.slackThread:gsub('%.', '')))
+        vim.fn.system(string.format(M.opts.terminal_cmd .. M.opts.argo_watch_cmd, selection.name, M.opts.argo_context,
+          selection.name))
       end)
-      actions.terminate_testrun = function()
+      actions.terminate_workflow = function()
         -- actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
-        vim.fn.system(string.format("kubectl --context %s delete testrun %s", M.staging_context, selection.name))
+        vim.fn.system(string.format("argo --context %s --namespace argo delete %s", M.orch_context, selection.name))
       end
+
+      map('n', 'x', actions.terminate_workflow)
+
       return true
     end,
   }):find()
 end
 
--- set_config_state("terminal_cmd", nil, "gnome-terminal --tab --title %s -- /usr/local/bin/forge shell %s")
+-- set_config_state("terminal_cmd", nil, "gnome-terminal --tab --title %s -- ")
+-- set_config_state("forge_shell_cmd", nil, "/usr/local/bin/forge shell %s")
 -- set_config_state("io_account", nil, "")
--- set_config_state("staging_context", nil, "gke_terraforged-66994cf9-acf8_us-west1_staging")
--- M.fraas_tests()
+-- set_config_state("staging_context", nil, "")
+-- set_config_state("argo_context", nil, "")
+-- set_config_state("argo_watch_cmd", nil, "/usr/local/bin/argo --context %s --namespace argo watch %s")
+-- M.fraas_argo()
 
 return telescope.register_extension {
   setup = function(ext_config)
-    set_config_state("terminal_cmd", ext_config.terminal_cmd,
-      "gnome-terminal --tab --title %s -- /usr/local/bin/forge shell %s")
+    set_config_state("terminal_cmd", ext_config.terminal_cmd, "gnome-terminal --tab --title %s -- ")
+    set_config_state("forge_shell_cmd", ext_config.forge_shell_cmd, "/usr/local/bin/forge shell %s")
     set_config_state("io_account", ext_config.io_account, "")
     set_config_state("sa_account", string.gsub(ext_config.io_account, "@", "-sa@"))
-    set_config_state("staging_context", ext_config.staging_context, "gke_terraforged-66994cf9-acf8_us-west1_staging")
+    set_config_state("staging_context", ext_config.staging_context, "")
+    set_config_state("argo_context", ext_config.argo_context, "")
+    set_config_state("argo_watch_cmd", ext_config.argo_watch,
+      "/usr/local/bin/argo --context %s --namespace argo watch %s")
   end,
   exports = {
     fraas = M.fraas_projects,
